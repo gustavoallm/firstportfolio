ea07013f4d1f0018a3c775a8a86e51e259bd9a95
gustavoallm <gustavo_allm@outlook.com>
2022-09-02 15:11:47 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var button = document.getElementById('actionBtn');
+
+button.addEventListener('click', function () {
+    var maisprojetos = document.getElementById('maisprojetos');
+
+    maisprojetos.classList.toggle('show');
+});
